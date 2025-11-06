314761e7a39d717b3ec9f71776a55047a5281abb
codingkr1s <suntev.kristian@gmail.com>
2025-11-06 13:17:21 +0100

    hallihallo

--- java-project/src/main/java/org/example/Main.java
+++ java-project/src/main/java/org/example/Main.java
@@ -9,7 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            TaskWordCounting.run(true);
+            TaskRoomSensorTelemetry.run(true);
+            //TaskWordCounting.run(true);
             //TaskExampleShowering.run(true);
         } catch (Exception e) {
             throw new RuntimeException(e);
